36acdc9011d9b11d7450cc0a1335a71ab4affe9d
Minoo Eftekhari <minoo.eftekhari5448@gmail.com>
2024-01-05 16:40:36 +0330

    Fetch the games

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
-export default axios.create({
-    params:{
-        baseURL:'https://api.rawg.io/api',
-        key:'c5d9b342e0504d23a426c2e31c1d16a5'
-    }
-})
+const apiClient = axios.create({
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "c5d9b342e0504d23a426c2e31c1d16a5",
+  },
+});
+
+export default apiClient;
