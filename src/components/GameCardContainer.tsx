6026d46d9e3c11a17f95b20cf0e182d73bc14f1e
Minoo Eftekhari <minoo.eftekhari5448@gmail.com>
2024-01-22 21:27:13 +0330

    Create a generic data fetching hook

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box width="auto" borderRadius={10} overflow="hidden">
+    <Box width="200px" borderRadius={10} overflow="hidden">
       {children}
     </Box>
   );
